ef221c0aa8350bf23d23926a2e8110c87e84f3aa
pradyuishere <you@yourpublicemail.example.com>
2019-04-08 11:36:57 +0530

    adding the alu

--- compiler.py
+++ compiler.py
@@ -5,25 +5,17 @@ mov_str  = '01'
 mvi_str1 = '00'
 mvi_str2 = '110'
 add_str  = '10000'
-adi_str  = '11000110'
 adc_str  = '10001'
-aci_str  = '11001110'
 sub_str  = '10010'
-sui_str  = '11010110'
 sbb_str  = '10011'
-sbi_str  = '11011110'
 inr_str1 = '00'
 inr_str2 = '100'
 dcr_str1 = '00'
 dcr_str2 = '101'
 ana_str  = '10100'
-ani_str  = '11100110'
 ora_str  = '10110'
-ori_str  = '11110110'
 xra_str  = '10101'
-xri_str  = '11101110'
 cmp_str  = '10111'
-cpi_str  = '11111110'
 rlc_str  = '00000111'
 rrc_str  = '00001111'
 ral_str  = '00010111'
@@ -57,36 +49,22 @@ try:
             str_out = adi_str+"{0:08b}".format(int(words[1]))
         if words[0]=='ADC':
             str_out = adc_str+"{0:03b}".format(int(words[1]))
-        if words[0]=='ACI':
-            str_out = aci_str+"{0:08b}".format(int(words[1]))
         if words[0]=='SUB':
             str_out = sub_str+"{0:03b}".format(int(words[1]))
-        if words[0]=='SUI':
-            str_out = sui_str+"{0:08b}".format(int(words[1]))
         if words[0]=='SBB':
             str_out = sbb_str+"{0:03b}".format(int(words[1]))
-        if words[0]=='SBI':
-            str_out = sbi_str+"{0:08b}".format(int(words[1]))
         if words[0]=='INR':
             str_out = inr_str1+"{0:03b}".format(int(words[1]))+inr_str2
         if words[0]=='DCR':
             str_out = dcr_str1+"{0:03b}".format(int(words[1]))+dcr_str2
         if words[0]=='ANA':
             str_out = ana_str+"{0:03b}".format(int(words[1]))
-        if words[0]=='ANI':
-            str_out = daa_str+"{0:08b}".format(int(words[1]))
         if words[0]=='ORA':
             str_out = ora_str+"{0:03b}".format(int(words[1]))
-        if words[0]=='ORI':
-            str_out = ori_str
         if words[0]=='XRA':
             str_out = xra_str+"{0:03b}".format(int(words[1]))
-        if words[0]=='XRI':
-            str_out = xri_str+"{0:08b}".format(int(words[1]))
         if words[0]=='CMP':
             str_out = cmp_str+"{0:03b}".format(int(words[1]))
-        if words[0]=='cpi':
-            str_out = cpi_str+"{0:08b}".format(int(words[1]))
         if words[0]=='RLC':
             str_out = rlc_str
         if words[0]=='RRC':
@@ -99,16 +77,10 @@ try:
             str_out = cma_str
         if words[0]=='CMC':
             str_out = cmc_str
-        if words[0]=='STC':
-            str_out = stc_str
         if words[0]=='JMP':
-            str_out = jmp_str+"{0:016b}".format(int(words[1]))
+            str_out = jmp_str+"{0:08b}".format(int(words[1]))
         if words[0]=='Jccc':
-            str_out = jccc_str1+"{0:03b}".format(int(words[1]))+jccc_str2+"{0:016b}".format(int(words[2]))
-        if words[0]=='RET':
-            str_out = ret_str
-        if words[0]=='Rccc':
-            str_out = rccc_str1+"{0:03b}".format(int(words[1]))+rccc_str2
+            str_out = jccc_str1+"{0:03b}".format(int(words[1]))+jccc_str2+"{0:08b}".format(int(words[2]))
         if words[0]=='HLT':
             str_out = hlt_str
         if words[0]=='NOP':
--- cpu.v
+++ cpu.v
@@ -9,7 +9,7 @@ module cpu(
 	);
 	
 	reg [7:0] this_instr;
-	reg [23:0] instr_mem [0:128];
+	reg [15:0] instr_mem [0:128];
 
 	reg program_counter;
 	integer state;
@@ -18,7 +18,21 @@ module cpu(
 	//state = 2 : perform operation
 	//state = 3 : save outputs
 	//state = 4 : update program_counter and send the outputs
-	reg instr;
+
+	reg carry;
+	reg borrow;
+	reg enable_alu;
+	reg input_ready_alu;
+	reg rst_in;
+
+	wire result_out_alu;
+	wire carry_out_alu;
+	wire borrow_out_alu;
+	wire result_ready_alu;
+	wire zero_out_alu;
+	wire negative_out_alu;
+	wire overflow_out_alu;
+	
 	reg signed [7:0] operand_A, operand_B;
 	reg signed [7:0] operand_A_alu, operand_B_alu;
 	reg [4:0] opcode_alu;
@@ -27,6 +41,25 @@ module cpu(
 	integer iterator;
 
 
+	module alu alu1(
+			.clk(clk),
+			.opcode(opcode_alu),
+			.operand_A(operand_A_alu),
+			.operand_B(operand_B_alu),
+			.enable(enable_alu),
+			.input_ready(input_ready_alu),
+			.carry_in(carry),
+			.borrow_in(borrow),
+			.rst(rst_in),
+			.result_out(result_out_alu),
+			.borrow_out(borrow_out_alu),
+			.result_ready(result_ready_alu),
+			.carry_out(carry_out_alu),
+			.zero(zero_out_alu),
+			.negative(negative_out_alu),
+			.overflow(overflow_out_alu)
+		);
+
 
 	always @(posedge clk or negedge rst_n) begin
 		if(reset == 1)
@@ -34,7 +67,10 @@ module cpu(
 			instr_mem <= 0;
 			program_counter <= 0;
 			state <= 0;
+			rst_in <= 1;
 
+			carry <= 0;
+			borrow <= 0;
 			for(iterator = 0; iterator < 8; iterator = iterator + 1)
 			begin
 				registers[iterator] <= 0;
@@ -42,33 +78,175 @@ module cpu(
 		end // If end
 		else	//else
 		begin
+			
+			rst_in <= 0;
+			enable_alu <= 1;
+
 			if(state==0)
 			begin
-				this_instr <= instr_men[program_counter][23:16];
-				operand_A <= instr_mem[program_counter][15:8];
-				operand_B <= instr_mem[program_counter][7:0];
+				this_instr <= instr_men[program_counter][15:8];
+				operand_A <= instr_mem[program_counter][7:0];
 
 				state <= 1;
 			end // if(state==0) end
 
 			if(state==1)
 			begin
-				if(this_instr[31:30]==2'b01)	//instr_mov reg to reg
+				state <= 2;
+
+				if(this_instr[7:6]==2'b01)	//instr_mov reg to reg
 				begin
-					registers[this_instr[29:27]] = registers[this_instr[26:24]];
+					registers[this_instr[5:3]] = registers[this_instr[2:0]];
+					state <= 0;
 				end
 
-				if(this_instr[31:30]==2'b00 && this_instr[26:24]==3'b110) //instr_mvi
+				if(this_instr[7:6]==2'b00 && this_instr[2:0]==3'b110) //instr_mvi
 				begin
-					registers[this_instr[29:27]] = operand_A;
+					registers[this_instr[5:3]] = operand_A;
+					state <= 0;
 				end
 
-				if(this_instr[31:27]==5'b10000)	//instr_add
+				if(this_instr[7:3]==5'b10000)	//instr_add
 				begin
-					operand_A_alu <= registers[this_instr[26:24]];
+					operand_A_alu <= registers[this_instr[2:0]];
 					operand_B_alu <= registers[0];
 					opcode_alu <= 0;
 				end
+
+				if(this_instr[7:3]==5'b10001)	//instr_addc
+				begin
+					operand_A_alu <= registers[0];
+					operand_B_alu <= registers[this_instr[2:0]];
+					opcode_alu <= 1;
+				end
+
+				if(this_instr[7:3]==5'b10010)	//instr_sub
+				begin
+					operand_A_alu <= registers[0];
+					operand_B_alu <= registers[this_instr[2:0]];
+					opcode_alu <= 2;
+				end
+
+				if(this_instr[7:3]==5'b10010)	//instr_subb
+				begin
+					operand_A_alu <= registers[0];
+					operand_B_alu <= registers[this_instr[2:0]];
+					opcode_alu <= 3;
+				end
+
+				if(this_instr[7:6]==2'b00 && this_instr[2:0]==3'b100) //instr_inr
+				begin
+					registers[this_instr[5:3]] <= operand_A;
+					opcode_alu <= 5;
+				end
+
+				if(this_instr[7:6]==2'b00 && this_instr[2:0]==3'b101) //instr_dcr
+				begin
+					registers[this_instr[5:3]] <= operand_A;
+					opcode_alu <= 6;
+				end
+
+				if(this_instr[7:3]==5'b10100)	//instr_ana
+				begin
+					operand_A_alu <= registers[this_instr[2:0]];
+					opcode_alu <= 8;
+				end				
+
+				if(this_instr[7:3]==5'b10110)	//instr_ora
+				begin
+					operand_A_alu <= registers[this_instr[2:0]];
+					opcode_alu <= 9;
+				end				
+
+				if(this_instr[7:3]==5'b10101)	//instr_xra
+				begin
+					operand_A_alu <= registers[this_instr[2:0]];
+					opcode_alu <= 8;
+				end				
+
+				if(this_instr[7:3]==5'b10111)	//instr_cmp
+				begin
+					operand_A_alu <= registers[0];
+					operand_B_alu <= registers[this_instr[2:0]];
+					opcode_alu <= 2;
+				end
+
+				if(this_instr[7:0]==8'b00000111)	//instr_rlc
+				begin
+					operand_A_alu <= registers[0];
+					opcode_alu <= 16;
+				end
+
+				if(this_instr[7:0]==8'b00001111)	//instr_rrc
+				begin
+					operand_A_alu <= registers[0];
+					opcode_alu <= 17;
+				end
+
+				if(this_instr[7:0]==8'b00010111)	//instr_ral
+				begin
+					operand_A_alu <= registers[0];
+					opcode_alu <= 18;
+				end
+
+				if(this_instr[7:0]==8'b00011111)	//instr_rar
+				begin
+					operand_A_alu <= registers[0];
+					opcode_alu <= 19;
+				end
+
+				if(this_instr[7:0]==8'b00101111)	//instr_cma
+				begin
+					operand_A_alu <= registers[0];
+					opcode_alu <= 11;
+				end
+
+				if(this_instr[7:0]==8'b00111111)	//instr_cmc
+				begin
+					state <= 0;
+					carry <= ~carry;
+				end
+
+				if(this_instr[7:0]==8'b11000011)	//instr_jmp
+				begin
+					program_counter <= operand_A;
+					state <= 0;
+				end
+
+				if(this_instr[7:6]==2'b11 && this_instr[2:0]==3'b010)
+				begin
+					if(registers[this_instr[5:3]] > 0)
+					begin
+						program_counter <= operand_A;
+					end
+					state <= 0;
+				end
+
+				if(this_instr[7:0]==8'b01110110)
+				begin
+					state <= 6;
+				end
+
+				if(this_instr[7:0]==8'b00000000)
+				begin
+					state <= 0;
+				end
+
+			end 	//state==1 end
+
+			if(state==2)
+			begin
+				if(result_ready_alu != 1)
+					input_ready_alu <= 1;
+				else
+				begin
+					state <= 3;
+				end
+			end 	//state==2 end
+
+			if(state == 3)
+			begin
+
 			end
 
 		end   //else end
